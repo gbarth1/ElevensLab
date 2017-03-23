a570680e480e17706c03e032b7aa73f17096e06c
gbarth1 <grace.m.barth@gmail.com>
2017-03-23 14:27:29 -0400

    hi

--- src/com/company/CardTester.java
+++ src/com/company/CardTester.java
@@ -1,5 +1,6 @@
 package com.company;
-
+import java.util.*;
+import java.io.*;
 /**
  * Created by gb647 on 3/15/17.
  */
@@ -10,6 +11,85 @@ public class CardTester {
         Card c1 = new Card("King", "hearts", 13);
         Card c2 = new Card("Queen", "spades", 12);
         Card c3 = new Card("jack", "hearts", 11);
+
+    public static Deck deal(){
+        Deck ret;
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> thing = new ArrayList();
+        System.out.println("what suits(say stop when done): ");
+        while(true){
+            System.out.println("enter suit: ");
+            String sss = sc.nextLine();
+            if(sss.equals("stop")){
+                break;
+            }
+            thing.add(sss);
+        }
+        System.out.println("What ranks?(say stop when done listing)");
+        String s="";
+        ArrayList<String> list = new ArrayList();
+        ArrayList<Integer> vallist = new ArrayList();
+        while(true){
+            System.out.println("enter rank: ");
+            s = sc.next();
+            if(s.equals("stop")){
+                break;
+            }
+            list.add(s);
+            System.out.println("enter that rank's value: ");
+            int i = sc.nextInt();
+            vallist.add(i);
+        }
+        int count = 0;
+        String[] list2 = new String[list.size()];
+        for(String ss:list){
+            if(count == vallist.size()-1){
+//	break;
+            }
+            list2[count] = ss;
+            count++;
+        }
+        count = 0;
+        int[] list3 = new int[list.size()];
+        for(int ss:vallist){
+            if(count == vallist.size()-1){
+//break;
+            }
+            list3[count] = ss;
+            count++;
+        }
+        count = 0;
+        String [] list1 = new String[thing.size()];
+        for(String w: thing){
+            list1[count] = w;
+            count++;
+        }
+
+
+        ret = new Deck(list1,list2,list3);
+        System.out.println("Custom cards?(y/n): ");
+        String a = sc.next();
+        if(a.equals("y")){
+            while(true){
+                System.out.println("custom card");
+                int i;
+                String s1,s2;
+                System.out.println("rank: ");
+                s1 = sc.next();
+                System.out.println("suit: ");
+                s2 = sc.next();
+                System.out.println("value(integer): ");
+                i = sc.nextInt();
+                ret.addCustom(s1,s2,i);
+                System.out.println("continue(y/n): ");
+                String t = sc.next();
+                if(t.equals("n")){
+                    break;
+                }
+
+            }
+        }
+        return ret;
 
     }
 }
--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -2,70 +2,36 @@ package com.company;
 import java.util.List;
 import java.util.ArrayList;
 
-/**
- * The Deck class represents a shuffled deck of cards.
- * It provides several operations including
- *      initialize, shuffle, deal, and check if empty.
- */
 public class Deck {
 
-    /**
-     * cards contains all the cards in the deck.
-     */
+
     private List<Card> cards;
-
-    /**
-     * size is the number of not-yet-dealt cards.
-     * Cards are dealt from the top (highest index) down.
-     * The next card to be dealt is at size - 1.
-     */
     private int size;
-
-
-    /**
-     * Creates a new <code>Deck</code> instance.<BR>
-     * It pairs each element of ranks with each element of suits,
-     * and produces one of the corresponding card.
-     * @param ranks is an array containing all of the card ranks.
-     * @param suits is an array containing all of the card suits.
-     * @param values is an array containing all of the card point values.
-     */
     public Deck(String[] ranks, String[] suits, int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		List cards = new ArrayList(size);
+        cards.add(ranks);
+        cards.add(suits);
+        cards.add(values);
     }
 
-
-    /**
-     * Determines if this deck is empty (no undealt cards).
-     * @return true if this deck is empty, false otherwise.
-     */
     public boolean isEmpty() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		if(cards.size()>0){
+            return false;
+        }else{
+            return true;
+        }
     }
 
-    /**
-     * Accesses the number of undealt cards in this deck.
-     * @return the number of undealt cards in this deck.
-     */
     public int size() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		return size;
     }
 
-    /**
-     * Randomly permute the given collection of cards
-     * and reset the size to represent the entire deck.
-     */
     public void shuffle() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 4 *** */
+
     }
 
-    /**
-     * Deals a card from this deck.
-     * @return the card just dealt, or null if all the cards have been
-     *         previously dealt.
-     */
     public Card deal() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		return cards.get(size);
     }
 
     /**
